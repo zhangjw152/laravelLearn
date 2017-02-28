1603494d04d22083ad89b8857df87a24534c7bbe
zhangjunwenmac <913620564@qq.com>
2017-02-28 21:52:47 +0800

    route and routeName

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//缺省是默认为'laravel'
+
+Route::get('/hello/name/{name?}',function ($name="laravel"){
+    return "hello {$name}";
+})->where('name','[A-Za-z]+');//参数的类型进行过滤
+//Route::match(['get','post'],'/hello',function(){
+//    return 'hello laravel';
+//});
+
+Route::get('/le/laravelAcademy/{name?}',['as'=>'academy',function ($name='zjw'){//为路由命名为academy
+    return "hello laravel {$name}";
+}]);
+Route::get('/testNameRoute/{name?}',function ($name='test'){//当没有name参数是输出的是zjw
+    return redirect()->route('academy',[$name]);//这里的academy前面不加/
+});
